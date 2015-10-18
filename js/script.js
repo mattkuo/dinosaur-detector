8948a6ac1c259899e544b7d69a75d548dcfb2fc6
Matthew Kuo <matthewkuo92@gmail.com>
2015-10-18 04:45:45 -0700

    Take picture and send to clairifai

--- js/script.js
+++ js/script.js
@@ -1,13 +1,38 @@
 // Javascript Code goes here
 $(document).ready(function() {
+  var firebaseRef = 'https://safe-dinosaur.firebaseio.com/';
   Webcam.attach( '#my-camera' );
 
   $('.snapshot').click(takeSnapshot);
 
   function takeSnapshot() {
-      Webcam.snap( function(dataUri) {
-          $('#my-result').append('<img src="'+dataUri+'"/>');
-      } );
+      // Webcam.snap(function(dataUri) {
+      //     var hash = CryptoJS.SHA256(Math.random() + CryptoJS.SHA256(dataUri));
+      //     var f = new Firebase(firebaseRef + 'pano/' + hash + '/filePayload');
+      //     f.set(dataUri, function(isError) {
+      //       if (isError === undefined) return;
+      //       console.log('success');
+      //       window.location.hash = hash;
+      //       $('#my-result').append('<img src="'+dataUri+'"/>');
+      //     });
+      // } );
+      Webcam.snap(function(dataUri) {
+        var rawImage = encodeURIComponent(dataUri.replace(/^data\:image\/\w+\;base64\,/, ''));
+        $.ajax({
+          type: 'POST',
+          url: 'https://api.clarifai.com/v1/tag/',
+          dataType: 'json',
+          headers: {
+            'Authorization': 'Bearer T01MKAlG9DySvJGuNw33jscBeOqQzC'
+          },
+          data: 'encoded_data=' + rawImage
+        })
+        .done(function(data) {
+          console.log(data.results[0].result.tag.classes);
+        });
+      });
+
+
   }
 
   // Create a connection to your Firebase database
