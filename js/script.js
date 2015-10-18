983b73fcda196cb6a4845e922489fed3748f8687
Matthew Kuo <matthewkuo92@gmail.com>
2015-10-17 23:16:07 -0700

    Webcam working

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
 // Javascript Code goes here
+$(document).ready(function() {
+  Webcam.attach( '#my_camera' );
+
+  function take_snapshot() {
+      Webcam.snap( function(data_uri) {
+          document.getElementById('my_result').innerHTML = '<img src="'+data_uri+'"/>';
+      } );
+  }
+
+});
