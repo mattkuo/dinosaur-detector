842cabf3b81fbe98286b43f1b0093affb6428c77
Denis Trailin <hiyou102@gmail.com>
2015-10-18 02:05:15 -0700

    added firebase demo

--- js/script.js
+++ js/script.js
@@ -10,4 +10,15 @@ $(document).ready(function() {
       } );
   }
 
+  // Create a connection to your Firebase database
+var ref = new Firebase("https://safe-dinosaur.firebaseio.com");
+
+// Save data
+ref.set({ name: "Alex Wolf" });
+
+// Listen for realtime changes
+ref.on("value", function(data) {
+  var name = data.val().name;
+});
+
 });
