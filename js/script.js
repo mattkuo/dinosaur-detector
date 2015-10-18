7288748f03bdfdb8eaf4936a0d054fe07999f76b
Matthew Kuo <matthewkuo92@gmail.com>
2015-10-17 23:32:04 -0700

    Take snapshot now works

--- js/script.js
+++ js/script.js
@@ -1,10 +1,12 @@
 // Javascript Code goes here
 $(document).ready(function() {
-  Webcam.attach( '#my_camera' );
+  Webcam.attach( '#my-camera' );
 
-  function take_snapshot() {
-      Webcam.snap( function(data_uri) {
-          document.getElementById('my_result').innerHTML = '<img src="'+data_uri+'"/>';
+  $('.snapshot').click(takeSnapshot);
+
+  function takeSnapshot() {
+      Webcam.snap( function(dataUri) {
+          $('#my-result').append('<img src="'+dataUri+'"/>');
       } );
   }
 
