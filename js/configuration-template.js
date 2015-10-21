51a1921c76cfa34997f093874c199c3ecd006836
Matthew Kuo <matthewkuo92@gmail.com>
2015-10-21 01:23:35 -0700

    Added separate files for api keys/base url

---
+++ js/configuration-template.js
@@ -0,0 +1,14 @@
+// Copy this file, fill in api keys and rename to configuration.js
+CONFIG = {
+  API_KEYS: {
+    clarifai: {
+      clientId: '',
+      clientSecret: ''
+    },
+    imgur: {
+      clientId: ''
+    }
+  },
+  // Base url for firebase
+  BASE_URL: ''
+};
